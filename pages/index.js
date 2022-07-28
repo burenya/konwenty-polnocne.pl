6afec086d8e9a815c6f0f895dc687ee7bb543ec3
Stanisław Dzioba <62724833+BoberITman@users.noreply.github.com>
2022-07-28 14:12:57 +0200

    SEO fix

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,13 @@ export default function Home() {
       <Head>
         <title>Konwenty Północne</title>
         <link rel="shortcut icon" href="/icon.svg" />
+        <meta name="description" content="Lista konwentów z pierwszej ręki!" />
+        <meta name="keywords" content="konwenty,konwenty północne,konwenty-północne,konpoly,konpóły,anime,manga,madzian,mahjong,fantasy,confamilia,zabenka"></meta>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://konwenty-polnocne.pl" />
+        <meta property="og:title" content="Konwenty Północne" />
+        <meta property="og:description" content="Lista konwentów z pierwszej ręki!" />
+        <meta property="og:image" content="https://konwenty-polnocne.pl/icon.png" />
       </Head>
       <Header />
       <section>{konwenty}</section>
