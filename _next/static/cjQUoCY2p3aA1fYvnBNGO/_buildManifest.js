a5b3b0d0e49e48fd1bcced5942454a261cfef52b
burenya-ci <burenya-ci@users.noreply.github.com>
2022-07-31 17:50:46 +0000

    Deploying to pages from @ burenya/konwenty-polnocne.pl@57ffe29378114dbf716e631a1846f6f8d5c57ac6 🚀

--- _next/static/cjQUoCY2p3aA1fYvnBNGO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4645e3d3595b8310.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
