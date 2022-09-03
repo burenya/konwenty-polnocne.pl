72ab2cbf09be4bac402a496ece3ce13866934fef
dzioba <stanislaw.dzioba@outlook.com>
2022-09-03 19:03:18 +0200

    IMplementing random styling

--- components/header.js
+++ components/header.js
@@ -1,10 +1,16 @@
-import { Zabenka } from "../icons/icons";
+import { Zabenka, Search } from "../icons/icons";
 
 export default function Home() {
   return (
-    <header className="bubble">
+    <header>
+      <div className="bubble">
         <Zabenka className="icon"></Zabenka>
         <p>Konwenty Północne</p>
+      </div>
+      <form className="bubble">
+        <Search className="icon"></Search>
+        <input type="search" placeholder="Szukaj" />
+      </form>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,36 @@
 import Header from "../components/header";
 import Foot from "../components/footer";
 import { Blob } from "../icons/icons";
+const colors = require("../data/config.json");
 const data = require("../data/lista.json");
 
 let konwenty = [];
 data.konwenty.forEach((x) => {
+  let rng_color = Math.floor(Math.random() * 10);
   konwenty.push(
-    <div className="konwent">
-      <div className="bottom">
-        <p className="name_bottom">{x.nazwa.toLocaleLowerCase()}</p>
-        <hr />
-        <p className="data_bottom">{x.data}</p>
-      </div>
+    <div
+      className="konwent"
+      style={{
+        "background-color": colors.colors[rng_color].bg_color,
+      }}
+    >
+      <h1>{x.nazwa.toLocaleUpperCase()}</h1>
+      <div className="div_footer">
+        <div className="bottom">
+          <p className="name_bottom">{x.nazwa.toLocaleLowerCase()}</p>
+          <hr />
+          <p className="data_bottom">{x.data}</p>
+        </div>
 
-      <div className="info">
-        <div>{x.miasto}</div>
-        <div>{x.typ}</div>
+        <div className="info">
+          <div>{x.miasto}</div>
+          <div>{x.typ}</div>
+        </div>
       </div>
-      <Blob className="blob"></Blob>
+      <Blob
+        className="blob"
+        style={{ color: colors.colors[rng_color].ac_color }}
+      ></Blob>
     </div>
   );
 });
