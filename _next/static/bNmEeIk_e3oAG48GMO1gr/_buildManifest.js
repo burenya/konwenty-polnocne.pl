cc0500ec5258bec84d6838959727f4e7d0c5c36f
kvpsky <kvpsky@users.noreply.github.com>
2022-10-13 18:46:08 +0000

    Deploying to pages from @ burenya/konwenty-polnocne.pl@57ffe29378114dbf716e631a1846f6f8d5c57ac6 🚀

--- _next/static/bNmEeIk_e3oAG48GMO1gr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-022d1f676e69bb98.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
