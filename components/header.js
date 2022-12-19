e3e77de1960faed33a5c88d41958e512f199f4f3
Stanislaw <62724833+BoberITman@users.noreply.github.com>
2022-12-19 17:59:41 +0100

    idk

--- components/header.js
+++ components/header.js
@@ -2,9 +2,12 @@ import { Zabenka } from "../icons/icons";
 
 export default function Home() {
   return (
-    <header className="bubble">
+    <header>
+      <div className="bubble">
         <Zabenka className="icon"></Zabenka>
         <p>Konwenty Północne</p>
+      </div>
+      <input className="bubble" type="text"></input>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,69 @@ import Header from "../components/header";
 import Foot from "../components/footer";
 import { Blob } from "../icons/icons";
 const data = require("../data/lista.json");
+const config = require("../assets/config.json");
 
+let months = [
+  "Styczeń",
+  "Luty",
+  "Marzec",
+  "Kwiecień",
+  "Maj",
+  "Czerwiec",
+  "Lipiec",
+  "Sierpień",
+  "Wrzesień",
+  "Październik",
+  "Listopad",
+  "Grudzień",
+];
 let konwenty = [];
-data.konwenty.forEach((x) => {
-  konwenty.push(
-    <div className="konwent">
-      <div className="bottom">
-        <p className="name_bottom">{x.nazwa.toLocaleLowerCase()}</p>
-        <hr />
-        <p className="data_bottom">{x.data}</p>
-      </div>
+let city = [];
+let month = [];
+let year = [];
+let type = [];
+function banner(konwent) {
+  let current = config.filter(function (e) {
+    return e.name == konwent.nazwa;
+  });
+  let img = [];
+  if (current[0] != undefined) {
+    for (let i = 0; i < current[0].count; i++) {
+      img.push(<img src={current[0].name.replaceAll(" ", "_") + i + ".svg"} />);
+    }
+    console.log(img);
+    return img;
+  }
+}
 
-      <div className="info">
-        <div>{x.miasto}</div>
-        <div>{x.typ}</div>
+data.konwenty.forEach((x) => {
+  const date = x.data.split(/[.]/g);
+
+  konwenty.push(
+    <div
+      className={
+        "konwent" +
+        " " +
+        x.typ +
+        " " +
+        x.miasto +
+        " " +
+        months[Number(date[1]) + 1] +
+        " " +
+        date[2]
+      }
+    >
+      <div className="banner">{banner(x)}</div>
+      <div className="content">
+        <div className="bottom">
+          <p className="name_bottom">{x.nazwa.toLocaleLowerCase()}</p>
+          <hr />
+          <p className="data_bottom">{x.data}</p>
+        </div>
+        <div className="info">
+          <div>{x.miasto}</div>
+          <div>{x.typ}</div>
+        </div>
       </div>
       <Blob className="blob"></Blob>
     </div>
@@ -25,6 +74,7 @@ export default function Home() {
   return (
     <>
       <Header />
+      <nav></nav>
       <section>{konwenty}</section>
       <Foot />
     </>
